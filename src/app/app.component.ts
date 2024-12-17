2649e2616e23f71cea2ceefd8793074de1e4d602
maxlongint <fillchar@hotmail.com>
2024-12-17 17:46:16 +0800

    跳转目录

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SessionStorage } from '@sleeko/utils/storage';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    @SessionStorage({ defaultValue: 0 })
+    count!: BehaviorSubject<number>;
+}
